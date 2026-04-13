086b29863366649d3028af4248231eacf35e510b
Gabriel <gmdf10@gmail.com>
2026-04-13 20:07:52 -0300

    criado método adicionar pedido

--- src/main/java/app/service/PedidoService.java
+++ src/main/java/app/service/PedidoService.java
@@ -11,4 +11,9 @@ public class PedidoService {
 
     private List<Pedido> pedidos = new ArrayList<>();
     private Map<Integer, Pedido> pedidosPorId = new HashMap<>();
+
+    public void adicionarPedido(Pedido pedido) {
+        pedidos.add(pedido);
+        pedidosPorId.put(pedido.id(), pedido);
+    }
 }
