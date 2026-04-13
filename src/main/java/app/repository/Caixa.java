0eb604ee294ac0c8ebe74d009ff0370c2bbc07b0
Gabriel <gmdf10@gmail.com>
2026-04-13 16:55:56 -0300

    criada classe caixa com seus métodos

---
+++ src/main/java/app/repository/Caixa.java
@@ -0,0 +1,14 @@
+package app.repository;
+
+public class Caixa<T> {
+
+    private T item;
+
+    public void guardar(T item) {
+        this.item = item;
+    }
+
+    public T abrir() {
+        return item;
+    }
+}
